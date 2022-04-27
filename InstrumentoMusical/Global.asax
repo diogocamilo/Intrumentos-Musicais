728b64443000be3ec38824a5b46431eceeb37012
Diogo Camilo <diogo.satos@interplayers.com.br>
2022-04-26 21:07:49 -0300

    Commit da configuração inicial da API.

---
+++ InstrumentoMusical/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InstrumentoMusical.WebApiApplication" Language="C#" %>
---
+++ Service/Services/MusicalInstrumentService.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using Repository.Contracts;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class MusicalInstrumentService : IMusicalInstrumentService
+    {
+        private readonly IMusicalInstrumentRepository musicalInstrumentRepository;
+
+        public MusicalInstrumentService(IMusicalInstrumentRepository musicalInstrumentRepository)
+        {
+            this.musicalInstrumentRepository = musicalInstrumentRepository;
+        }
+
+        public async Task<List<MusicalInstrument>> GetAll()
+        {
+            return await musicalInstrumentRepository.GetAll();
+        }
+    }
+}
