bb9182f8416454e3f3752205a4024ca3571a382e
brodripa <biancarodrigues_paulino@hotmail.com>
2022-05-08 15:04:06 -0300

    nova classe

---
+++ Domain/Models/TipoInstrumento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Domain.Models
+{
+    public abstract class TipoInstrumento
+    {
+        public string NomeProduto { get; set; }
+        public abstract string NumeroProduto(int valor, string nomeProduto);
+
+        public string FormatarSelecao()
+        {
+            const string chars = "ABCasDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            NumeroProduto = new string(Enumerable.Repeat(chars, 15)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return NomeProduto;
+        }
+    }
+}
