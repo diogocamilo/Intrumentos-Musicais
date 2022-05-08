ad10ea16b69bc7e26efc34ba3d9fdf7f30884cd9
brodripa <biancarodrigues_paulino@hotmail.com>
2022-05-08 15:09:39 -0300

    Novas classes

---
+++ Domain/Models/Cordas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Models
+{
+    public class Cordas : TipoInstrumento
+    {
+        public override string Selecionar(int valor, string nomeProduto)
+        {
+            return FormatarSelecao();
+        }
+    }
+}
---
+++ Domain/Models/Idiofone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Models
+{
+    public class Idiofone
+    {
+        public override string Selecionar(int valor, string nomeProduto)
+        {
+            return FormatarSelecao();
+        }
+    }
+}
